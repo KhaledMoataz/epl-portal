9c8eee17c5ffd84d836cb9d0236a8e3c7abaf4f7
Khaled Nassar <khaled.nassar99@eng-st.cu.edu.eg>
2021-01-24 02:55:28 +0200

    initial navbar trial

---
+++ src/common/NavigationBar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Nav, Navbar } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+    .navbar {
+        background-color: #222;
+    }
+    a,
+    .navbar-brand,
+    .navbar-nav .nav-link {
+        color: #bbb;
+        &:hover {
+            color: white;
+        }
+    }
+`;
+
+const NavigationBar = () => (
+    <Styles>
+        <Navbar expand="lg">
+            <Navbar.Brand href="/">EPL</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Item>
+                        <Nav.Link>
+                            <Link to="/">Home</Link>
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link>
+                            <Link to="/reservation">Reservation</Link>
+                        </Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    </Styles>
+);
+
+export default NavigationBar;
---
+++ src/reservation/Reservation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Reservation = () => (
+    <div>
+        <h2>Reservation</h2>
+    </div>
+);
+
+export default Reservation;
