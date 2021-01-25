92f76c784f09a98ac232d0737444aead94f40330
Ayman <ayman.elakwah@gmail.com>
2021-01-25 22:56:57 +0200

    reservation style

--- src/reservation/ReservationTable.js
+++ src/reservation/ReservationTable.js
@@ -3,17 +3,17 @@ import { Button } from 'react-bootstrap';
 import './reservation.css';
 
 const BookButton = () => (
-    <Button variant="outline-primary" size="xxl">
+    <Button className="reservation-button" variant="outline-primary" size="xxl">
         Book
     </Button>
 );
 const UnbookButton = () => (
-    <Button variant="outline-danger" size="xxl">
+    <Button className="reservation-button" variant="outline-danger" size="xxl">
         Unbook
     </Button>
 );
 const BookedButton = () => (
-    <Button variant="dark" size="xxl" disabled>
+    <Button className="reservation-button" variant="dark" size="xxl" disabled>
         Booked
     </Button>
 );
@@ -24,25 +24,27 @@ const isBooked = [
 ];
 
 const ReservationTable = () => (
-    <div className="text-center">
-        {isBooked.map((row, rowIndex) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <div key={rowIndex}>
-                {row.map((chair, colIndex) => {
-                    const auxKey = rowIndex * row.length + colIndex;
-                    switch (chair) {
-                        case -1:
-                            return <BookedButton key={auxKey} />;
-                        case 0:
-                            return <BookButton key={auxKey} />;
-                        case 1:
-                            return <UnbookButton key={auxKey} />;
-                        default:
-                            return <BookButton key={auxKey} />;
-                    }
-                })}
-            </div>
-        ))}
+    <div className="reservation-bg">
+        <div className="reservation-window">
+            {isBooked.map((row, rowIndex) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <div className="reservation-row" key={rowIndex}>
+                    {row.map((chair, colIndex) => {
+                        const auxKey = rowIndex * row.length + colIndex;
+                        switch (chair) {
+                            case -1:
+                                return <BookedButton key={auxKey} />;
+                            case 0:
+                                return <BookButton key={auxKey} />;
+                            case 1:
+                                return <UnbookButton key={auxKey} />;
+                            default:
+                                return <BookButton key={auxKey} />;
+                        }
+                    })}
+                </div>
+            ))}
+        </div>
     </div>
 );
 
