1b9132930413ba8349e91a972a91ba799ef8bd37
Ayman Azzam <aymanazzam63@gmail.com>
2021-01-29 00:56:55 +0200

    Connect Handlers

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import Item from './Item';
 
-const Admin = () => (
-    <div className="container-fluid py-3">
-        <div className="row">
-            <div className="col-6">
-                <Item columnNum="1" />
-            </div>
+const Admin = () => {
+    const handleDelete = () => {
+        console.log('Delete');
+    };
 
-            <div className="col-6">
-                <Item columnNum="2" />
+    const handleAccept = () => {
+        console.log('Accept');
+    };
+
+    const handleCancel = () => {
+        console.log('Cancel');
+    };
+
+    return (
+        <div className="container-fluid py-3">
+            <div className="row">
+                <div className="col-6">
+                    <Item
+                        Cancel={handleCancel}
+                        Accept={handleAccept}
+                        username="aymanazzam"
+                        columnNum="1"
+                    />
+                </div>
+
+                <div className="col-6">
+                    <Item Delete={handleDelete} username="aymanazzam2" columnNum="2" />
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Admin;
--- src/Admin/Item.js
+++ src/Admin/Item.js
@@ -3,24 +3,24 @@ import React from 'react';
 const Item = (props) => {
     const oneItem = (
         <div className="row px-3 py-3">
-            <div className="col">username</div>
+            <div className="col-7">{props.username}</div>
 
             {props.columnNum === '1' ? (
                 <>
                     <div>
-                        <button className="btn btn-secondary" type="submit">
+                        <button onClick={props.Cancel} className="btn btn-secondary" type="button">
                             Cancel
                         </button>
                     </div>
-                    <div className="col-3">
-                        <button className="btn btn-primary" type="submit">
+                    <div className="col">
+                        <button onClick={props.Accept} className="btn btn-primary" type="button">
                             Accept
                         </button>
                     </div>
                 </>
             ) : (
-                <div className="col-3">
-                    <button className="btn btn-danger" type="submit">
+                <div className="col">
+                    <button onClick={props.Delete} className="btn btn-danger" type="button">
                         Delete
                     </button>
                 </div>
