136fdc59ee7f012d28f2c19ff04669a2c9d9ddd3
Ayman Azzam <aymanazzam63@gmail.com>
2021-01-28 23:04:21 +0200

    Make admin page with two elements

---
+++ src/Admin/Admin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Admin = () => (
+    <div className="container-fluid py-3">
+        <div className="row">
+            <div className="text-center col-6">List for Managers with accept or cancel</div>
+
+            <div className="text-center col-6">List for All users with delete</div>
+        </div>
+    </div>
+);
+
+export default Admin;
